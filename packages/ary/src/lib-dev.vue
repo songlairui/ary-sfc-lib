646fb73e307159cd150935aecd031240d77f63f8
songlairui <songlairui@126.com>
2019-12-18 09:01:24 +0800

    feat(add lib): sfc-init

---
+++ packages/ary/src/lib-dev.vue
@@ -0,0 +1,23 @@
+<script>
+import { ArySample } from '@/entry';
+export default {
+    name:  'AryLibDev', // vue library dev component
+    components: {
+        ArySample,
+    },
+}
+</script>
+
+<template>
+    <div class="ary-lib-dev">
+        <ary-sample></ary-sample>
+    </div>
+</template>
+
+<style scoped>
+    .ary-lib-dev {
+        display: block;
+        width: 400px;
+        margin: 25px auto;
+    }
+</style>
